1d38ed546cd8363432c7a3f92a1d539f64e9cc89
suramyadas01 <41816183+suramyadas01@users.noreply.github.com>
2020-04-19 12:15:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,30 @@
+# Before starting to code, it is recommended that you look up the documentation related to the pynput library. It is easy to use and 
+# simple to understand.
+
+#
+
 import os
 import subprocess
 import webbrowser
 import serial
 from pynput.mouse import Controller, Button
 mouse = Controller()
+#The Controller() lets us create a mouse object that we can later use to emulate the mouse.
+#You may want to include the keyboard functions too but the problem is both the objects will call Controller() and cause an error.
+#I believe there are ways around it but I didn't want the keyboard anyway so I did not think about it much.
+
 ser = serial.Serial('/dev/ttyACM0', baudrate = 9600, timeout = 1)
+# Here, the '/dev/ttyACM0' will not be same for you. Replace with wha0tever your serial port is.
+# NOTE: You may have to edit this part if you disconnect your arduino from the computer. Check the serial port via the 
+#	arduino IDE.
 
 while True:	
 	data = ser.readline().decode('ascii')
 	print(data)
+	#Just reading if any new data is available on the serial line.
+	#Now we can just use an if-else ladder for various functions. 
+	
+	#MOVING THE MOUSE CURSOR
 	if data == 'UP\r\n':
 		mouse.move(0, -20)
 		print("UP")
@@ -26,6 +42,8 @@ while True:
 		mouse.move(-20, 0)
 	elif data == 'RIGHT\r\n':
 		mouse.move(20, 0)
+	
+	#EMULATING MOUSE BUTTON FUNCTIONS
 	elif data == 'OK\r\n':
 		mouse.press(Button.left)
 		mouse.release(Button.left)
@@ -37,9 +55,12 @@ while True:
 		mouse.scroll(0, -3)
 	elif data == 'SC_UP\r\n':
 		mouse.scroll(0, 3)	
+		
+		
+	#Lastly, we use the dedicated buttons to open Youtube and Netflix directly via a button on the remote. 
+	#Surely, you can change the URLs and modify the entire thing.  
 	elif data == 'YOUTUBE\r\n':
 		webbrowser.open('http://www.youtube.com')
 	elif data == 'NETFLIX\r\n': 
 		webbrowser.open('http://www.netflix.com')
-	elif data == 'SPOTIFY':
-		open('/home/aymar01/Videos')
+	
